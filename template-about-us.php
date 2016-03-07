32d3cfaa8254c84ab0fbd558aff3dfa07bf78fd5
tierbria <briac.tier@mail.utoronto.ca>
2016-03-07 12:16:48 -0500

    Added files via upload
    adding css and template for the 'about us' page

---
+++ template-about-us.php
@@ -0,0 +1,41 @@
+<?php
+/**
+*Template Name: About Us
+*
+*@package Fluffy
+*/
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php 
+				while ( have_posts() ) : the_post(); 
+				// start the Loop
+			?>
+
+			<div class="entry-content-about">
+					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>		
+					<?php the_post_thumbnail(); ?>
+					<?php the_content();?>
+			</div>
+
+			<?php
+				endwhile; 
+				//end the Loop
+			?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
