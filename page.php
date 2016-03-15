0268003bbd727d1fc0bbdaf927840f5aa328194d
tierbria <briac.tier@mail.utoronto.ca>
2016-03-15 09:37:36 -0400

    added page template

--- page.php
+++ page.php
@@ -1,38 +1,49 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package _s
- */
+*Template Name: About Us
+*
+* @package btn
+*/
 
 get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php
-			while ( have_posts() ) : the_post();
+		<?php if ( have_posts() ) : ?>
 
-				get_template_part( 'template-parts/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
+			<?php 
+				while ( have_posts() ) : the_post(); 
+				// start the Loop
 			?>
+
+			<div class="entry-content-about">
+					<?php 
+					//Gets the page title
+					the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+					<?php 
+					//Gets the page thumbnail
+					the_post_thumbnail(); 
+					?>
+					<?php 
+					//Gets the page content
+					the_content();
+					?>
+			</div>
+
+			<?php
+				endwhile; 
+				//end the Loop
+			?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
